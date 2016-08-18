f4681d0c8409ed8c888e653474d7969fc9637812
AndrewScalise <paradigmcomplex@gmail.com>
2016-08-18 14:11:08 -0500

    Finish studio5

--- main.py
+++ main.py
@@ -98,19 +98,33 @@ class AddMovie(webapp2.RequestHandler):
     """
 
     def post(self):
+
         # look inside the request to figure out what the user typed
         new_movie = self.request.get("new-movie")
+        # TODO 1
+        # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
+        new_movie = cgi.escape(new_movie, quote=True)
 
         # TODO 2
         # if the user typed nothing at all, redirect and yell at them
+        if not new_movie:
+            # make a helpful error message
+            error = "Type something fool!"
+            error_escaped = cgi.escape(error, quote=True)
 
+            # redirect to homepage, and include error as a query parameter in the URL
+            self.redirect("/?error=" + error_escaped)
 
         # TODO 3
         # if the user wants to add a terrible movie, redirect and yell at them
+        if new_movie in terrible_movies:
+            # make a helpful error message
+            error = "That's a terrible movie!"
+            error_escaped = cgi.escape(error, quote=True)
 
+            # redirect to homepage, and include error as a query parameter in the URL
+            self.redirect("/?error=" + error_escaped)
 
-        # TODO 1
-        # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
 
         # build response content
         new_movie_element = "<strong>" + new_movie + "</strong>"
